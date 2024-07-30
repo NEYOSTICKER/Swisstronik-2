ad47169ddcffe0c704a67f4fd35895114aa5f730
Neyosticker <ainaneyo@gmail.com>
2024-07-30 12:14:36 +0000

    Initial commit

--- contracts/ZunXBT.sol
+++ contracts/ZunXBT.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ZunXBT is ERC721, Ownable {
-    constructor(address initialOwner) ERC721("POND NFT", "POND") Ownable(initialOwner) {}
+    constructor(address initialOwner) ERC721("NEYO", "NEY") Ownable(initialOwner) {}
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
